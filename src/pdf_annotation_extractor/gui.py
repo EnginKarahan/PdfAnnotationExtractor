b1cee0b61cfbf824b1f716251fa4471e0dd48157
Engin Karahan <50199063+EnginKarahan@users.noreply.github.com>
2024-12-02 23:45:53 +0100

    Update gui.py - Copyright

--- src/pdf_annotation_extractor/gui.py
+++ src/pdf_annotation_extractor/gui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 PDF Annotation Extractor - Graphical user interface
-Copyright (C) 2024  [Ihr Name]
+Copyright (C) 2024 Engin Karahan - https://karahan.net
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
